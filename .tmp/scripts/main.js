49316b0b1626fd41e4c5e6cd8fc81591728d8ce8
Gene <genehan01@gmail.com>
2017-01-29 20:08:06 -0800

    Add photoswipe and scaffolding

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -57,5 +57,33 @@ $(window).load(function () {
   skrollr.init({
     refresh: $('body')
   });
+
+  function openPhotoSwipe() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [{
+      src: 'http://placehold.it/350x150',
+      w: 600,
+      h: 400
+    }, {
+      src: 'http://placehold.it/350x150',
+      w: 1200,
+      h: 900
+    }];
+
+    // define options (if needed)
+    var options = {
+      // optionName: 'option value'
+      // for example:
+      index: 0 // start at first slide
+    };
+
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  }
+
+  document.getElementById('btn').onclick = openPhotoSwipe;
 });
 //# sourceMappingURL=main.js.map
